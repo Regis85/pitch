ea660121bc4a04b6c0c2f2c05058a5c98933949c
Regis85 <regis.bouguin@laposte.net>
2024-06-26 23:40:36 +0200

    Update identifiants.php

--- identifiants.php
+++ identifiants.php
@@ -1,5 +1,5 @@
 <?php
-            $servername = "localhost";
-            $username = "pitch_user";
-            $password = "@2hZkGnY]H@OsoWe";
-            $myDB = "pitchgolf";
+            $servername = "";
+            $username = "";
+            $password = "";
+            $myDB = "";
